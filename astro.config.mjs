573a38a24a9efffc81463080c58c44ebd9902f13
dongguacute <gudupao2022@gmail.com>
2025-08-29 15:31:11 +0800

    feat(i18n): add international

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,19 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
+import { astroI18nPlugin } from '@gudupao/astro-i18n';
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
   vite: {
     plugins: [tailwindcss()],
 },
 
-  integrations: [react()],
+  integrations: [
+    astroI18nPlugin({
+      localesDir: './locales',
+      fallbackLang: 'en',
+      pathBasedRouting: false
+    }),
+    react()],
 });
